7ba8f8193f890b75dec1fc07ec0486f0166464d7
nlea <nuhlemann@uni-potsdam.de>
2020-04-08 10:11:05 +0200

    Adding test for "abgelehnt-Path"

--- src/test/java/io/remedymatch/engine/ProzessJUnitTest.java
+++ src/test/java/io/remedymatch/engine/ProzessJUnitTest.java
@@ -7,6 +7,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
 
@@ -23,9 +26,10 @@ public class ProzessJUnitTest {
     @Test
     @Deployment(resources = "bpmn/angebotAnfrageProzess.bpmn")
     public void testHappyPath() {
-        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("angebot_anfrage_prozess");
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("angebot_anfrage_prozess", withVariables("institution", "Camunda"));
 
         assertThat(processInstance).isWaitingAt("angebot_anfrage_prozess_beantworten");
+        assertThat(task()).hasCandidateGroup("Camunda");
         complete(task(), withVariables("angenommen", true));
 
         assertThat(processInstance).isWaitingAt("angebot_anfrage_prozess_match_prozess_starten").
@@ -33,6 +37,28 @@ public class ProzessJUnitTest {
         complete(externalTask());
 
         assertThat(processInstance).isEnded().hasPassed("MatchProzessGestartetEndEvent");
+
+
+    }
+
+
+    @Test
+    @Deployment(resources = "bpmn/angebotAnfrageProzess.bpmn")
+    public void testAnfrageAbegelehnt() {
+        Map<String, Object> abgelehntMap = new HashMap<String, Object>();
+        abgelehntMap.put("angenommen", false);
+
+        ProcessInstance processInstance = runtimeService()
+                .createProcessInstanceByKey("angebot_anfrage_prozess")
+                .setVariables(abgelehntMap)
+                .startAfterActivity("angebot_anfrage_prozess_beantworten")
+                .execute();
+
+        assertThat(processInstance).isWaitingAt("angebot_anfrage_prozess_stornierung_verarbeiten").
+                externalTask().hasTopicName("angebotAnfrageAblehnen");
+        complete(externalTask());
+
+        assertThat(processInstance).isEnded().hasPassed("AngebotStorniertEnde");
 
 
     }
